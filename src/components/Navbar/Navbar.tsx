163cb769383b9f6ad25d0303fb16289fe84d07a9
pl3lee <billy.pl.lee@gmail.com>
2024-08-23 23:18:10 +0000

    created navbar and about section

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import styles from "./Navbar.module.scss";
+
+export function Navbar() {
+  return (
+    <nav className={styles.root}>
+      <ul className={styles.linksContainer}>
+        <li>
+          <Link href="/experience">experience</Link>
+        </li>
+        <li>
+          <Link href="/projects">projects</Link>
+        </li>
+        <li>
+          <Link href="/education">education</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
