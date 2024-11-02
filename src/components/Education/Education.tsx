b702bf8134677fe8aa15d6e5461665b3852dc241
pl3lee <billy.pl.lee@gmail.com>
2024-11-02 16:58:40 +0000

    done with education

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,24 +1,27 @@
 import styles from './Education.module.scss';
-import { SectionHeader } from '../SectionHeader/SectionHeader';
-import Link from 'next/link';
 
 export function Education() {
   return (
     <>
       <div className={styles.educationContainer}>
-        <div>
-          <h2>Georgia Institute of Technology</h2>
+        <h2>Georgia Institute of Technology</h2>
+        <div className={styles.detailsContainer}>
           <div className='text-sm'>Master of Science in Computer Science</div>
+          <div className='text-sm'>Computing Systems Specialization</div>
           <div className='text-sm'>Expected Jun 2026</div>
         </div>
       </div>
       <div className={styles.educationContainer}>
-        <div>
-          <h2>University of Waterloo</h2>
+        <h2>University of Waterloo</h2>
+        <div className={styles.detailsContainer}>
           <div className='text-sm'>Honours Bachelor of Mathematics</div>
           <div className='text-sm'>
-            Double major in Computational Mathematics and Combinatorics &
-            Optimization. Minor in Computer Science
+            Double Major in Computational Mathematics and Combinatorics &
+            Optimization
+          </div>
+          <div className='text-sm'>Minor in Computer Science</div>
+          <div className='text-sm'>
+            Dean&apos;s Honours List (88.5% Average)
           </div>
           <div className='text-sm'>Sept 2020 - Aug 2024</div>
         </div>
